4bdc21151f682caec069747ea5fb58e1fc64ee72
ernestotieno95@gmail.com <ernest.otieno@student.moringaschool.com>
2024-07-07 23:46:23 +0300

    publish

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+const changeLeastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer() {
+    changeLeastFavoriteCustomer = 'some new value';
+}
